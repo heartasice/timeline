1ea0662b34022d2b8eb8d42aa3178ad7a03a01f6
Miya <ciutirc@gmail.com>
2016-03-03 14:54:32 +0700

    ID-ID

--- dw-timeline/templates/content-none.php
+++ dw-timeline/templates/content-none.php
@@ -1,10 +1,10 @@
 <article id="post-0" class="hentry post no-results not-found dwtl full">
   <div class="entry-inner">
     <header>
-      <h2 class="entry-title"><?php _e( 'Nothing Found', 'dw-timeline' ); ?></h2>
+      <h2 class="entry-title"><?php _e( 'Tidak ada apa-apa', 'dw-timeline' ); ?></h2>
     </header>
     <div class="entry-content">
-      <p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.', 'dw-timeline' ); ?></p>
+      <p><?php _e( 'Tidak menemukan apa apa, maaf...', 'dw-timeline' ); ?></p>
 			<?php get_search_form(); ?>
     </div>
   </div>
